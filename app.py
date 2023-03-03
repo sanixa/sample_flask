d5de4edcd45695c1cff74bf3adf40615920f0220
sanixa <cegg12345678@yahoo.com.tw>
2023-03-03 16:37:10 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import os
+from flask import Flask, request, Response
+from flask_cors import CORS 
+import yaml
+
+with open('config/dev.yaml', 'r',encoding="utf-8") as f:
+    config = yaml.load(f, Loader=yaml.Loader)
+
+
+app = Flask(__name__, static_folder='static')
+cors = CORS(app) 
+
+
+@app.route('/sample', methods=['GET', 'POST'])
+def sample_func():
+
+    res = {
+        'code': 200,
+        'msg': 'success'
+    }
+    return Response(response=json.dumps(res, indent = 4), status=200, content_type='application/json')
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
